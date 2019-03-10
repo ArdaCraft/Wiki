e425ce4147213077f96b664d2dbbaee57d7df29d
Michael Anderson <mikeandeeson@gmail.com>
2019-03-09 19:30:08 -0700

    don't want lang

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -12,13 +12,12 @@ class Footer extends React.Component {
     const baseUrl = this.props.config.baseUrl;
     const docsUrl = this.props.config.docsUrl;
     const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    return `${baseUrl}${docsPart}${langPart}${doc}`;
+    return `${baseUrl}${docsPart}${doc}`;
   }
 
   pageUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language ? `${language}/` : '') + doc;
+    return baseUrl + doc;
   }
 
   render() {
