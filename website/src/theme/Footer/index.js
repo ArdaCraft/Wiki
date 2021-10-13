1bdea908abf87ad7590a3dfef9c972c64f89aba7
Freshmilkymilk <fresh@freshy.me>
2021-10-12 22:06:07 -0600

    correct copyright

--- website/src/theme/Footer/index.js
+++ website/src/theme/Footer/index.js
@@ -18,7 +18,7 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/" style={{ textDecoration: 'none' }}>
+      <Link color="inherit" href="https://ardacraft.me" style={{ textDecoration: 'none' }}>
         ArdaCraft
       </Link>{' '}
       {new Date().getFullYear()}
